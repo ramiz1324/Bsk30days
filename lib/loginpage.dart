7c346d31eb9f9ced9e9f1c2dbb44e62e7e4a0a4b
Sk Ramiz <ramizcodeup004@gmail.com>
2022-07-16 22:19:44 +0530

    day8 complete

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -2,98 +2,178 @@
 import 'package:bsk_flutter_1/register.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formkey = GlobalKey<FormState>();
+  movetohome(BuildContext context) async {
+    if (formkey.currentState!.validate()) {
+      setState(() {
+        change = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, '/register');
+      setState(() {
+        change = false;
+      });
+    }
+  }
+
+  String name = "";
+  bool change = false;
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            "assets/images/login_image.png",
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Text(
-            "Welcome",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
+      child: Form(
+        key: formkey,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/hey.png",
+              fit: BoxFit.cover,
             ),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 16),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "User Name", hintText: "Enter User Name"),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      labelText: "Password", hintText: "Enter Password"),
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 0, horizontal: 85),
-                  child: Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                textStyle: TextStyle(
-                                  fontStyle: FontStyle.italic,
-                                ),
-                                elevation: 10,
-                                shadowColor: Colors.amber,
-                                side: BorderSide(
-                                    color: Colors.amber, width: 0.8)),
-                            onPressed: () {
-                               Navigator.pushNamed(context, '/register');;
-                            },
-                            child: Text(
-                              "Login",
-                              style: TextStyle(color: Colors.amber),
-                            )),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            side: BorderSide(color: Colors.amber, width: 0.8),
-                            shadowColor: Colors.amber,
-                            elevation: 10,
-                          ),
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/singup');
-                          },
-                          //style: ElevatedButton.styleFrom(
-                          //primary: Colors.brown
-                          //),
-
-                          child: Text(
-                            "Register",
-                            style: TextStyle(color: Colors.amber),
-                          ),
-                        ),
-                      ],
-                    ),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              "Welcome $name",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 16),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "User Name", hintText: "Enter User Name"),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Username cannot be Empty ";
+                      }
+                    },
                   ),
-                )
-              ],
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: "Password",
+                        hintText: "Enter Password",
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.purple))),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Password cannot be Empty ";
+                      } else if (value.length < 6) {
+                        return "Password lenght should be al least 6";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 0, horizontal: 85),
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Material(
+                            borderRadius:
+                                BorderRadius.circular(change ? 50 : 8),
+                            color: Colors.purple,
+                            child: InkWell(
+                              splashColor: Colors.green,
+                              onTap: () => movetohome(context),
+                              child: AnimatedContainer(
+                                duration: Duration(seconds: 1),
+                                height: 40,
+                                width: change ? 50 : 100,
+                                alignment: Alignment.center,
+                                child: change
+                                    ? Icon(
+                                        Icons.done,
+                                        color: Colors.yellowAccent,
+                                      )
+                                    : Text(
+                                        "login",
+                                        style: TextStyle(
+                                            color: Color.fromARGB(
+                                                255, 196, 192, 182),
+                                            fontSize: 17,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                // decoration: BoxDecoration(
+                                //     color: Color.fromARGB(255, 92, 60, 148),
+                                //     borderRadius:
+                                //         BorderRadius.circular(change ? 30 : 9)),
+                              ),
+                            ),
+                          ),
+                          // ElevatedButton(
+                          //   style: ElevatedButton.styleFrom(
+                          //       primary: Color.fromARGB(221, 113, 88, 161),
+                          //       textStyle: TextStyle(
+                          //         fontStyle: FontStyle.italic,
+                          //       ),
+                          //       elevation: 10,
+                          //       shadowColor: Colors.amber,
+                          //       side:
+                          //           BorderSide(color: Colors.amber, width: 0.8)),
+                          //   onPressed: () {
+                          //     Navigator.pushNamed(context, '/register');
+                          //     ;
+                          //   },
+                          //   child: Text(
+                          //     "Login",
+                          //     style: TextStyle(color: Colors.amber),
+                          //   ),
+                          // ),
+                          // ElevatedButton(
+                          //   style: ElevatedButton.styleFrom(
+                          //     primary: Color.fromARGB(221, 113, 88, 161),
+                          //     side: BorderSide(color: Colors.amber, width: 0.8),
+                          //     shadowColor: Colors.amber,
+                          //     elevation: 10,
+                          //   ),
+                          //   onPressed: () {
+                          //     Navigator.pushNamed(context, '/singup');
+                          //   },
+                          //   //style: ElevatedButton.styleFrom(
+                          //   //primary: Colors.brown
+                          //   //),
+
+                          //   child: Text(
+                          //     "Register",
+                          //     style: TextStyle(color: Colors.amber),
+                          //   ),
+                          // ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/register.dart
+++ lib/register.dart
@@ -1,209 +1,230 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage2 extends StatefulWidget {
   @override
   _HomePage2State createState() => _HomePage2State();
 }
 
 class _HomePage2State extends State<HomePage2> {
+  String root = "";
+  final secondkey = GlobalKey<FormState>();
+  mini(BuildContext context) {
+    if (secondkey.currentState!.validate()) {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         resizeToAvoidBottomInset: false,
         body: Padding(
           padding: const EdgeInsets.only(left: 6.0, right: 6.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                child: Stack(
-                  children: <Widget>[
-                    Container(
-                    padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
-                     child: InkWell(
-                       onTap: (){
-                       },
-                      child: Text(
-                        "SignUP",
-                        style: TextStyle(
-                          fontSize: 80.0,
-                          fontWeight: FontWeight.bold,
-                          //color: Colors.green,
-                        ),
-                      ),
-                     ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(255.0, 118.0, 0.0, 0.0),
-                      child: Text(
-                        ".",
-                        style: TextStyle(
-                          fontSize: 80.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.green,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "EMAIL",
-                    labelStyle: TextStyle(
-                      fontFamily: "Montserrat",
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green)),
-                  ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "PASSWORD",
-                    labelStyle: TextStyle(
-                      fontFamily: "Montserrat",
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green)),
-                  ),
-                  obscureText: true,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "USER NAME",
-                    labelStyle: TextStyle(
-                      fontFamily: "Montserrat",
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green)),
-                  ),
-                ),
-              ),
-              SizedBox(height: 40.0),
-              Padding(
-                padding: const EdgeInsets.only(right: 20, left: 20),
-                child: Container(
-                  height: 50.0,
-                  child: Material(
-                    borderRadius: BorderRadius.circular(20.0),
-                    shadowColor: Colors.greenAccent,
-                    color: Colors.green,
-                    elevation: 7.0,
-                    child: GestureDetector(
-                        onTap: () {},
-                        child: Center(
+          child: Form(
+            key: secondkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                        child: InkWell(
+                          onTap: () {},
                           child: Text(
-                            "SINGUP",
+                            "SignUP $root",
                             style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: "Montserrat",
+                              fontSize: 80.0,
                               fontWeight: FontWeight.bold,
+                              //color: Colors.green,
                             ),
                           ),
-                        )),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(255.0, 118.0, 0.0, 0.0),
+                        child: Text(
+                          ".",
+                          style: TextStyle(
+                            fontSize: 80.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              SizedBox(height: 20.0),
-              Padding(
-                padding: const EdgeInsets.only(right: 20, left: 20),
-                child: Container(
-                  height: 50.0,
-                  color: Colors.transparent,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        style: BorderStyle.solid,
-                        width: 1.0,
+                Container(
+                  padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                  child: TextFormField(
+                    onChanged: ((value) {
+                      root = value;
+                      setState(() {});
+                    }),
+                    decoration: InputDecoration(
+                      labelText: "EMAIL",
+                      labelStyle: TextStyle(
+                        fontFamily: "Montserrat",
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
                       ),
-                      color: Colors.transparent,
-                      borderRadius: BorderRadius.circular(20.0),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green)),
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        // Center(
-                        //   child: ImageIcon(AssetImage('assets/facebook.png')),
-                        // ),
-                        Center(
-                          child: InkWell(
-                            onTap: () {
-                              Navigator.pop(context);
-                            },
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Enter UserName";
+                      }
+                    },
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "PASSWORD",
+                      labelStyle: TextStyle(
+                        fontFamily: "Montserrat",
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green)),
+                    ),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Enter UserName";
+                      } else if (value.length < 6) {
+                        return "Password should be 6 lenght";
+                      }
+                    },
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "USER NAME",
+                      labelStyle: TextStyle(
+                        fontFamily: "Montserrat",
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green)),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: Container(
+                    height: 50.0,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(20.0),
+                      shadowColor: Colors.greenAccent,
+                      color: Colors.green,
+                      elevation: 7.0,
+                      child: GestureDetector(
+                          onTap: () => mini(context),
+                          child: Center(
                             child: Text(
-                              "Go Back",
+                              "SINGUP",
                               style: TextStyle(
-                                color: Colors.black,
+                                color: Colors.white,
                                 fontFamily: "Montserrat",
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
-                          ),
-                        )
-                      ],
+                          )),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 125.0,
-              ),
-              Container(
-                child: InkWell(
-                  onTap: (){},
-                  child:Text("© RIS 2021 All right reserved. By Sk Ramiz ",
-                style: TextStyle(color: Colors.red,
-                fontFamily: "Times new Roman"
+                SizedBox(height: 20.0),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: Container(
+                    height: 50.0,
+                    color: Colors.transparent,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          style: BorderStyle.solid,
+                          width: 1.0,
+                        ),
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          // Center(
+                          //   child: ImageIcon(AssetImage('assets/facebook.png')),
+                          // ),
+                          Center(
+                            child: InkWell(
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text(
+                                "Go Back",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: "Montserrat",
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-                
+                SizedBox(
+                  height: 125.0,
                 ),
-                )
-              )
-              
-              // Row(
-              //    mainAxisAlignment: MainAxisAlignment.center,
-              //   children:<Widget> [
+                Container(
+                    child: InkWell(
+                  onTap: () {},
+                  child: Text(
+                    "© RIS 2021 All right reserved. By Sk Ramiz ",
+                    style: TextStyle(
+                        color: Colors.red, fontFamily: "Times new Roman"),
+                  ),
+                ))
 
-              //     Text('New To Spotify ?',
-              //      style: TextStyle(
-              //                 color: Colors.black,
-              //                 fontFamily: "Montserrat",
-              //                 //fontWeight: FontWeight.bold,
-              //     ),
-              //     ),
-              //     SizedBox(
-              //       width: 10.0,
-              //     ),
-              //     InkWell(
-              //       onTap: (){},
-              //       child: Text('Register Here',
-              //        style: TextStyle(
-              //                 color: Colors.redAccent,
-              //                 fontFamily: "Montserrat",
-              //                 fontWeight: FontWeight.bold,
-              //                 decoration: TextDecoration.underline
-              //                 ),
-              //     ),
-              //     ),
-              //   ],
-              // )
-            ],
+                // Row(
+                //    mainAxisAlignment: MainAxisAlignment.center,
+                //   children:<Widget> [
+
+                //     Text('New To Spotify ?',
+                //      style: TextStyle(
+                //                 color: Colors.black,
+                //                 fontFamily: "Montserrat",
+                //                 //fontWeight: FontWeight.bold,
+                //     ),
+                //     ),
+                //     SizedBox(
+                //       width: 10.0,
+                //     ),
+                //     InkWell(
+                //       onTap: (){},
+                //       child: Text('Register Here',
+                //        style: TextStyle(
+                //                 color: Colors.redAccent,
+                //                 fontFamily: "Montserrat",
+                //                 fontWeight: FontWeight.bold,
+                //                 decoration: TextDecoration.underline
+                //                 ),
+                //     ),
+                //     ),
+                //   ],
+                // )
+              ],
+            ),
           ),
         ));
   }
